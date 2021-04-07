759c8d134137485be11e01d932ef7d684733fa6d
Luigi Antelmi <luigi.antelmi@gmail.com>
2021-04-07 13:30:51 +0200

    runscript from everywhere

--- random_citation.sh
+++ random_citation.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-cat ./books/*.md | grep "^-" | shuf | head -n1 | sed 's/\. /\.\n/g' | sed 's/, /,\n/g'
+WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cat "${WD}"/books/*.md | grep "^-" | shuf | head -n1 | sed 's/\. /\.\n/g' | sed 's/, /,\n/g'
