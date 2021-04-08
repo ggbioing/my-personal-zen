4f6dfce9e977bfec1bbc9702dcbcb8cc29c69592
Luigi Antelmi <luigi.antelmi@gmail.com>
2021-04-08 09:28:49 +0200

    upd

--- random_citation.sh
+++ random_citation.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cat "${WD}"/books/*.md | grep "^-" | shuf | head -n1 | sed 's/\. /\.\n/g' | sed 's/, /,\n/g'
+echo
+cat "${WD}"/books/*.md | grep "^-" | shuf | head -n1 | sed -E 's/([\.,:;])/\1\n/g'
